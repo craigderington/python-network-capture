09f9e07bb62e7a7247ab674172245f9eb070fa52
Craig Derington <craig@craigderington.me>
2017-01-11 11:47:08 -0500

    Add README.md and add source files.

---
+++ README.md
@@ -0,0 +1,26 @@
+##### Python3: Network Packet Capture
+
+This Python project is based on the packet sniffer tutorial by thenewboston.
+
+https://www.youtube.com/watch?v=WGJC5vT5YJo
+
+Our packet sniffer captures ethernet frames and displays the destination and source MAC addresses and the protocol being used.
+
+Usage:
+
+```
+$ time python3 packet_sniffer.py
+```
+
+Output:
+
+```
+Ethernet Frame: 
+Destination: FF:FF:FF:FF:FF:FF, Source: 24:A4:3C:30:36:2B, Protocol: 1544
+
+Ethernet Frame: 
+Destination: AC:72:89:93:47:C0, Source: 00:15:5D:00:24:0E, Protocol: 8
+
+```
+
+
---
+++ packet_sniffer.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python3
+
+import socket
+import struct
+import textwrap
+
+
+def main():
+    """
+    A simple Python network packet sniffer based on the tutorial
+    from thenewboston
+    :param mac address
+    :type network packet
+    :return: string
+    """
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame: ')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+
+
+# unpack ethernet frames
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+
+# return a properly formatted mac address, i.e. (AA:BB:CC:DD:EE:FF)
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
+
+
+# unpack ipv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+
+# return a properly formatted ipv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+
+if __name__ == '__main__':
+    main()
