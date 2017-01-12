e5b98a7f272f6891ac7943d400006f047fbfd10a
Craig Derington <craig@craigderington.me>
2017-01-12 16:58:12 -0500

    Add additional methods for unpacking data frames.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -9,7 +9,7 @@ def main():
     """
     A simple Python network packet sniffer based on the tutorial
     from thenewboston
-    :param mac address
+    :param  1 mac address
     :type network packet
     :return: string
     """
@@ -46,6 +46,41 @@ def ipv4_packet(data):
 # return a properly formatted ipv4 address
 def ipv4(addr):
     return '.'.join(map(str, addr))
+
+
+# unpack icmp packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum
+
+
+# unpack tcp segment
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack('! H H L H H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+
+# unpack udp segment
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+
+# format the multi-line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(bytes) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join(prefix + line for line in textwrap.wrap(string, size))
 
 
 if __name__ == '__main__':
