0c2db7c9ed260ef196571b9bfdd993067f49438f
Craig Derington <craig@craigderington.me>
2017-01-17 09:23:16 -0500

    Update methods and add tab spacing.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -5,6 +5,17 @@ import struct
 import textwrap
 
 
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t '
+DATA_TAB_2 = '\t\t '
+DATA_TAB_3 = '\t\t\t '
+DATA_TAB_4 = '\t\t\t\t '
+
+
 def main():
     """
     A simple Python network packet sniffer based on the tutorial
@@ -19,7 +30,47 @@ def main():
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame: ')
-        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+        print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+
+        # ipv4
+        if eth_proto == 8:
+            (version, header_length, ttl, proto, src, target, data) = ipv4_packet(data)
+            print(TAB_1 + 'IPv4 Packet: ')
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(version, header_length, ttl))
+            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
+
+            if proto == 1:
+                icmp_type, code, checksum, data = icmp_packet(data)
+                print(TAB_1 + 'ICMP Packet:')
+                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {}', format(icmp_type, code, checksum))
+                print(TAB_2 + 'Data:')
+                print(format_multi_line(DATA_TAB_3, data))
+
+            elif proto == 6:
+                (src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, \
+                 flag_syn, flag_fin) = tcp_segment(data)
+                print(TAB_1 + 'TCP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(src_port, dest_port))
+                print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(sequence, acknowledgment))
+                print(TAB_2 + 'Flags:')
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}, RST: {}, SYN: {}, FIN: {}'.format(flag_urg, flag_ack, \
+                                                                                            flag_psh, flag_rst, \
+                                                                                            flag_syn, flag_fin))
+                print(TAB_2 + 'Data:')
+                print(format_multi_line(DATA_TAB_3, data))
+
+            elif proto == 17:
+                src_port, dest_port, length, data = udp_segment(data)
+                print(TAB_1 + 'UDP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(src_port, dest_port, length))
+
+            else:
+                print(TAB_1 + 'Data:')
+                print(format_multi_line(DATA_TAB_2, data))
+
+        else:
+            print('Data:')
+            print(format_multi_line(DATA_TAB_1, data))
 
 
 # unpack ethernet frames
@@ -51,7 +102,7 @@ def ipv4(addr):
 # unpack icmp packet
 def icmp_packet(data):
     icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
-    return icmp_type, code, checksum
+    return icmp_type, code, checksum, data[4:]
 
 
 # unpack tcp segment
